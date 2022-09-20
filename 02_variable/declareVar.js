edf6f57d7ab35e7996504ea049215c98b64c992b
augustinLib <2bambitious@gmail.com>
2022-09-21 02:05:00 +0900

    Feat: Add 02_variable

---
+++ 02_variable/declareVar.js
@@ -0,0 +1,12 @@
+var userId = 1;
+var userName = "Lee"
+
+
+// 객체나 배열과 같은 자료구조를 통해 여러 값을 하나의 값처럼 사용
+var user = {id: 1, name : "Lee"};
+
+var users = [
+    {id: 1, name: 'Lee'},
+    {id: 2, name: 'Kim'}
+];
+
