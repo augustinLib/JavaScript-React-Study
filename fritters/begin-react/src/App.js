bd633010b8e26727d858b541622db300771b6902
orange-fritters <mindongss@snu.ac.kr>
2022-11-09 20:10:41 +0900

    오공완

--- fritters/begin-react/src/App.js
+++ fritters/begin-react/src/App.js
@@ -1,31 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import Hello from './Hello';
+import InputSample from './InputSample.js';
 
 function App() {
   return (
-      <>
-        <Hello />
-        <div>안녕히계세요</div>
-        <div>hihi</div>
-      </>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <InputSample />
   );
 }
 
