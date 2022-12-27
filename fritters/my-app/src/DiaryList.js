5c35fc2509109f2f217f63aa816100f20c20055f
orange-fritters <mindongss@snu.ac.kr>
2022-12-28 01:58:37 +0900

    오공완

--- fritters/my-app/src/DiaryList.js
+++ fritters/my-app/src/DiaryList.js
@@ -1,13 +1,13 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ diaryList }) => {
+const DiaryList = ({ onRemove, onEdit, diaryList }) => {
   return (
     <div className="DiaryList" align="center">
       <h2>일기 목록</h2>
       <h3>{diaryList.length}개의 일기가 있습니다</h3>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} />
+          <DiaryItem key={it.id} {...it} onEdit={onEdit} onRemove={onRemove} />
         ))}
       </div>
     </div>
